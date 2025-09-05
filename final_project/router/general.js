d96b7dc229348047bf5bc8b09244f24daf5eb6be
Ralphdany <ralphnjambou@gmail.com>
2025-09-05 10:54:32 -0400

    user registration added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,10 +5,25 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body
+
+       // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!isValid(username)) {
+            // Add the new user to the users array
+            users.push({username, password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user. Please provide username and password"});
+})
+
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -58,7 +73,10 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params
+  const reviews = books[isbn].reviews
+
+  return res.status(200).json(reviews);
 });
 
 module.exports.general = public_users;
