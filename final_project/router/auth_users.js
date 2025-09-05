06a958224dfbf35d25ab8486018894a53b5daffd
Ralphdany <ralphnjambou@gmail.com>
2025-09-05 11:17:06 -0400

    added user sign in

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -36,9 +36,33 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
+
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const { username, password } = req.body
+
+  // Check if username or password is missing
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+}
+
+// Authenticate user
+if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign({
+        data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    // Store access token and username in session
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.status(200).send({
+        message: "User successfully logged in", accessToken
+    });
+} else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+}
+})
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
